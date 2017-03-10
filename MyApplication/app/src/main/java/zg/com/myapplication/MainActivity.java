c13f7e4bd5c688808568d780493c74b9ec3adb30
zg <1075120274@qq.com>
2017-03-10 19:40:28 +0800

    啊啊

---
+++ MyApplication/app/src/main/java/zg/com/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package zg.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View viewById = findViewById(R.id.activity_main);
+    }
+}
